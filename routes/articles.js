551211ecfbdbfe3e0ccb75a9293bb78e9f527fb4
Jennifer Davila <pazjenni1331@gmail.com>
2021-12-19 19:23:53 -0600

    bug in code - looking into how to fix.

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const Article = require("./../models/articles");
 const router = express.Router();
 
 router.get("/new", (req, res) => {
   res.render("articles/new");
+});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", async (req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+  try {
+    article = await article.save();
+    res.redirect(`/articles/${article.id}`);
+  } catch (err) {
+    res.render("articles/new", { article: article });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const app = express();
 const articleRouter = require("./routes/articles");
 
 // this single line connects you to the database - can be named whatever you want.
-mongoose.connect("mongodb://localhose/blog");
+mongoose.connect("mongodb://localhose/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
 app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
